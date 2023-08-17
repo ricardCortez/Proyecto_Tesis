7303edab753a0555782dedb73d93df2082ba7b15
ricardCortez <cn83y42r9smhr2938h@>
2023-08-16 22:52:34 -0500

    se modifico la vista del docente y personal admin, solo podran actualizar su clave.

--- static/js/update_user.js
+++ static/js/update_user.js
@@ -1,148 +1,163 @@
 $(document).ready(function() {
+    let originalValues = {};
+
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+    }
+
+    function updateOriginalValues() {
+        originalValues = {
+            nombre: $("#nombre_input").val(),
+            apellido_paterno: $("#apellido_paterno_input").val(),
+            apellido_materno: $("#apellido_materno_input").val(),
+            correo_electronico: $("#correo_electronico_input").val(),
+            celular: $("#celular_input").val(),
+            fecha_nacimiento: $("#fecha_nacimiento_input").val()
+        };
+    }
 
     function toggleEdit() {
-        let isEditing = $("#edit_update_button").text() === "Actualizar";
+        const isEditing = $("#edit_update_button").text() === "Actualizar";
 
         if (isEditing) {
-            // Recoger los valores de los campos editados
-            let dataToUpdate = {
+            // Si se está editando, actualizamos los datos
+            const dataToUpdate = {
                 dni: $("#numero_documento_text").text(),
-                nombre: $("#nombre_input").val(),
-                apellido_paterno: $("#apellido_paterno_input").val(),
-                apellido_materno: $("#apellido_materno_input").val(),
-                correo_electronico: $("#correo_electronico_input").val(),
-                celular: $("#celular_input").val(),
-                fecha_naimiento: $("#fecha_naimiento_input").val()
+                ...originalValues
             };
 
-            // Enviar una solicitud al servidor para actualizar los datos
-            $.post("/update_usuario", dataToUpdate)
-                .done(function(data) {
+            $.post('/update_usuario', dataToUpdate)
+                .done(function() {
                     Swal.fire({
                         icon: 'success',
                         title: 'Éxito',
-                        text: 'Datos actualizados correctamente.',
+                        text: 'Datos actualizados correctamente.'
                     });
                 })
                 .fail(function() {
                     Swal.fire({
                         icon: 'error',
                         title: 'Error',
-                        text: 'Ocurrió un error al actualizar los datos.',
+                        text: 'Ocurrió un error al actualizar los datos.'
                     });
                 });
 
-            // Cambiar el texto del botón
             $("#edit_update_button").text("Editar");
-
-            // Ocultar el botón de cancelar
             $("#cancel_button").hide();
-            // Mostrar los spans y ocultar los inputs
             $("[id$='_text']").show();
             $("[id$='_input']").hide();
 
         } else {
-            // Mostrar los inputs y ocultar los spans
+            // Si no se está editando, pasamos a modo edición
             $("[id$='_text']").hide();
             $("[id$='_input']").show();
-
-            // Cambiar el texto del botón
             $("#edit_update_button").text("Actualizar");
-
-            // Mostrar el botón de cancelar
             $("#cancel_button").show();
         }
     }
 
     function cancelEdit() {
-        // Mostrar los spans y ocultar los inputs
+        for (const key in originalValues) {
+            $(`#${key}_input`).val(originalValues[key]);
+        }
         $("[id$='_text']").show();
         $("[id$='_input']").hide();
-
-        // Cambiar el texto del botón de edición
         $("#edit_update_button").text("Editar");
-
-        // Ocultar el botón de cancelar
         $("#cancel_button").hide();
     }
 
     function searchUserByDNI() {
-        var dni_usuario = $("#search_dni").val();
+        const dni_usuario = $("#search_dni").val();
 
-        $.post("/buscar_usuario_por_dni", { dni: dni_usuario })
+        $.post('/buscar_usuario_por_dni', { dni: dni_usuario })
             .done(function(data) {
-                // Si la solicitud es exitosa, actualiza los campos del formulario con los resultados de la búsqueda
-                $("#tipo_perfil_text").text(data.tipo_perfil);
-                $("#numero_documento_text").text(data.numero_documento);
-                $("#nombre_text").text(data.nombre);
-                $("#apellido_paterno_text").text(data.apellido_paterno);
-                $("#apellido_materno_text").text(data.apellido_materno);
-                $("#correo_electronico_text").text(data.correo_electronico);
-                $("#celular_text").text(data.celular);
-                $("#fecha_naimiento_text").text(data.fecha_naimiento);
+                for (const key in data) {
+                    if (key === "fecha_nacimiento") {
+                        $(`#${key}_text`).text(formatDate(data[key]));
+                        $(`#${key}_input`).val(formatDate(data[key]));
+                    } else if (key === "clave_asignada") {
+                        // Si es la contraseña, mostramos asteriscos
+                        $(`#${key}_text`).text("*****");
+                    } else {
+                        $(`#${key}_text`).text(data[key]);
+                        $(`#${key}_input`).val(data[key]);
+                    }
+                }
 
-                // Mostrar el div con clase .field que contiene los resultados
                 $(".field").show();
-
-                // Mostrar el botón "Editar/Actualizar"
                 $("#edit_update_button").show();
+
+                updateOriginalValues();
             })
             .fail(function() {
                 Swal.fire({
                     icon: 'error',
                     title: 'Error',
-                    text: 'Error en la búsqueda o no se encontró un usuario con ese DNI.',
+                    text: 'Error en la búsqueda o no se encontró un usuario con ese DNI.'
                 });
             });
     }
-function changePassword() {
-        let newPassword = $("#newPassword").val();
-        let repeatPassword = $("#repeatPassword").val();
+
+    function changePassword() {
+        const newPassword = $("#newPassword").val();
+        const repeatPassword = $("#repeatPassword").val();
 
         if (newPassword !== repeatPassword) {
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
-                text: 'Las contraseñas no coinciden.',
+                text: 'Las contraseñas no coinciden.'
             });
             return;
         }
 
-        // Aquí enviamos la contraseña al servidor para actualizarla
-        $.post("/update_usuario", { clave_asignada: newPassword })
-            .done(function(data) {
+        $.post('/update_usuario', { clave_asignada: newPassword })
+            .done(function() {
                 Swal.fire({
                     icon: 'success',
                     title: 'Éxito',
-                    text: 'Contraseña actualizada correctamente.',
+                    text: 'Contraseña actualizada correctamente.'
                 });
-                // Cerrar la modal
                 $("#passwordModal").hide();
             })
             .fail(function() {
                 Swal.fire({
                     icon: 'error',
                     title: 'Error',
-                    text: 'Ocurrió un error al actualizar la contraseña.',
+                    text: 'Ocurrió un error al actualizar la contraseña.'
                 });
             });
     }
 
-    // Mostrar la modal cuando se hace clic en el botón de cambio de contraseña
-    $("#changePassword").click(function() {
+    // Inicializamos valores originales
+    updateOriginalValues();
+
+    // Event bindings
+    $("#search_button").click(searchUserByDNI);
+    $("#edit_update_button").click(toggleEdit);
+    $("#cancel_button").click(cancelEdit);
+    $("#changePassword").click(function() { $("#passwordModal").show(); });
+    $("#closeModal").click(function() { $("#passwordModal").hide(); });
+    $("#savePassword").click(changePassword);
+});
+
+// Modal functionality
+$(document).ready(function() {
+    // Open the modal
+    $("#changePasswordButton").click(function() {
         $("#passwordModal").show();
     });
 
-    // Cerrar la modal cuando se hace clic en el botón de cierre
+    // Close the modal
     $("#closeModal").click(function() {
         $("#passwordModal").hide();
     });
 
-    // Llamar a la función changePassword cuando se hace clic en el botón de guardar en la modal
-    $("#savePassword").click(changePassword);
-
-    // Resto de los eventos
-    $("#search_button").click(searchUserByDNI);
-    $("#edit_update_button").click(toggleEdit);
-    $("#cancel_button").click(cancelEdit);
+    // Close the modal when clicking outside of the modal content
+    $(window).click(function(event) {
+        if ($(event.target).is("#passwordModal")) {
+            $("#passwordModal").hide();
+        }
+    });
 });
--- static/js/view_user.js
+++ static/js/view_user.js
@@ -1,72 +1,8 @@
+
 document.addEventListener("DOMContentLoaded", function() {
-
-    const buttons = document.querySelectorAll("button");
-
-    // Logic for "Cancelar" buttons
-    const cancelButtons = document.querySelectorAll(".cancel-button");
-    cancelButtons.forEach(button => {
-        button.addEventListener("click", function(event) {
-            event.preventDefault();
-            const fieldName = this.previousElementSibling.previousElementSibling.name;
-            const textElement = document.getElementById(`${fieldName}_text`);
-            const inputElement = document.getElementById(`${fieldName}_input`);
-
-            // Reset input value to the original text value
-            inputElement.value = textElement.textContent;
-
-            // Switch back to display mode
-            textElement.style.display = "inline";
-            inputElement.style.display = "none";
-            this.previousElementSibling.textContent = "Editar";
-            this.style.display = "none";  // Hide the "Cancelar" button
-        });
-    });
-
-
-    buttons.forEach(button => {
-        button.addEventListener("click", function(event) {
-            event.preventDefault();
-            const previousSibling = this.previousElementSibling;
-if (!previousSibling) return;
-const fieldName = previousSibling.name;
-            const textElement = document.getElementById(`${fieldName}_text`);
-            const inputElement = document.getElementById(`${fieldName}_input`);
-
-            // Si el campo es "clave_asignada", muestra el modal
-            if (fieldName === "clave_asignada") {
-                showPasswordModal();
-                return;
-            }
-
-            // Si el inputElement no existe o el campo no es editable, simplemente retorna
-            if (!inputElement) {
-                return;
-            }
-
-            if (inputElement.style.display === "none" || inputElement.style.display === "") {
-                textElement.style.display = "none";
-                inputElement.style.display = "inline";
-                inputElement.focus();
-                this.textContent = "Guardar";
-                this.nextElementSibling.style.display = "inline";
-            } else {
-                updateUser(fieldName, inputElement.value, function(success) {
-                    if (success) {
-                        textElement.textContent = inputElement.value;
-                        textElement.style.display = "inline";
-                        inputElement.style.display = "none";
-                        button.textContent = "Editar";
-                    } else {
-                        Swal.fire("Error", "Error al actualizar", "error");
-                    }
-                });
-            }
-        });
-    });
-
     const passwordModal = document.getElementById('passwordModal');
-    const changePasswordButton = document.getElementById('changePassword');
     const closeModalButton = document.getElementById('closeModal');
+    const changePasswordButton = document.getElementById('changePassword');
     const savePasswordButton = document.getElementById('savePassword');
 
     function showPasswordModal() {
@@ -85,48 +21,62 @@ const fieldName = previousSibling.name;
         showPasswordModal();
     });
 
-    savePasswordButton.addEventListener('click', function() {
-        const newPassword = document.getElementById('newPassword').value;
-        const repeatPassword = document.getElementById('repeatPassword').value;
+   function changePassword() {
+        const newPassword = $("#newPassword").val();
+        const repeatPassword = $("#repeatPassword").val();
 
-        if (newPassword === repeatPassword) {
-            if (validatePassword(newPassword)) {
-                updateUser('clave_asignada', newPassword, function(success) {
-                    if (success) {
-                        document.getElementById('clave_asignada_text').textContent = '******';
-                        hidePasswordModal();
-                        Swal.fire("Éxito", "Contraseña actualizada con éxito", "success");
-                    } else {
-                        Swal.fire("Error", "Error al actualizar contraseña", "error");
-                    }
-                });
-            } else {
-                Swal.fire("Error", "La contraseña no cumple con los requisitos.", "error");
-            }
-        } else {
-            Swal.fire("Error", "Las contraseñas no coinciden.", "error");
+        if (newPassword !== repeatPassword) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Las contraseñas no coinciden.'
+            });
+            return;
         }
+
+        $.post('/update_usuario', { clave_asignada: newPassword })
+            .done(function() {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Éxito',
+                    text: 'Contraseña actualizada correctamente.'
+                });
+                $("#passwordModal").hide();
+            })
+            .fail(function() {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'Ocurrió un error al actualizar la contraseña.'
+                });
+            });
+    }
+
+    // Inicializamos valores originales
+    updateOriginalValues();
+
+    // Event bindings
+    $("#changePassword").click(function() { $("#passwordModal").show(); });
+    $("#closeModal").click(function() { $("#passwordModal").hide(); });
+    $("#savePassword").click(changePassword);
+});
+
+// Modal functionality
+$(document).ready(function() {
+    // Open the modal
+    $("#changePasswordButton").click(function() {
+        $("#passwordModal").show();
     });
 
-    function validatePassword(password) {
-        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
-        return regex.test(password);
-    }
+    // Close the modal
+    $("#closeModal").click(function() {
+        $("#passwordModal").hide();
+    });
 
-    function updateUser(field, value, callback) {
-        fetch('/actualizar_usuario', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                field: field,
-                value: value
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            callback(data.success);
-        });
-    }
+    // Close the modal when clicking outside of the modal content
+    $(window).click(function(event) {
+        if ($(event.target).is("#passwordModal")) {
+            $("#passwordModal").hide();
+        }
+    });
 });
